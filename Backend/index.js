b3f84f3ae469658f7888e21a1f51662aeea7875d
Kanel04 <thelegendanelka@gmail.com>
2023-03-18 17:13:53 +0300

    backend prot running and dashboard page

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,27 @@
-console.log("hello world!")
+// Core Modules
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "config.env" });
+const cors = require("cors");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+const env = require("env-var");
+const PORT = env.get("PORT").required().asInt();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+  
+
+// Server listen PORT
+server.listen(PORT, () => {
+    console.log("listening on *:", PORT);
+  });
+
+process.on("unhandledRejection", (err, promise) => {
+console.log(`Logged Error: ${err.message}`);
+   server.close(() => process.exit(1));
+});
